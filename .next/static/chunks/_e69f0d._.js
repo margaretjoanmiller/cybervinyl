61b15647eef53a5cd289ad38c6d446b1ac7afa69
Margaret Miller <mmiller@wanderingwires.net>
2025-02-08 06:17:11 -0800

    Setting up nuxt

---
+++ .next/static/chunks/_e69f0d._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/_e69f0d._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/_e69f0d._.js",
+  "chunks": [
+    "static/chunks/cbc35_next_dist_compiled_react-dom_7838ad._.js",
+    "static/chunks/cbc35_next_dist_compiled_5b8a11._.js",
+    "static/chunks/cbc35_next_dist_client_fca81a._.js",
+    "static/chunks/cbc35_next_dist_e8e16e._.js",
+    "static/chunks/e3def_@swc_helpers_cjs_1f8947._.js",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_d40af8._.js"
+  ],
+  "source": "entry"
+});
