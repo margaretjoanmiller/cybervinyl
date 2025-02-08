b9fe2d8427cf2513ee7e301b3a106316369b2e3c
Margaret Miller <mmiller@wanderingwires.net>
2025-02-08 03:39:32 -0800

    Basic nuxt app

---
+++ .next/static/chunks/src_app_page_tsx_c95dbc._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_c95dbc._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_c95dbc._.js",
+  "chunks": [
+    "static/chunks/cbc35_next_dist_a97e22._.js",
+    "static/chunks/src_app_page_tsx_8b213b._.js"
+  ],
+  "source": "dynamic"
+});
